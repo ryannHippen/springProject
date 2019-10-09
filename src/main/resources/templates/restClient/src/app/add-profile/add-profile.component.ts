e30839e381b2d4fe37ea09a3e9fa3c63e04e5a1a
Owen Koehler <owen.koehler@tcs.com>
2019-10-09 10:25:03 -0400

    fixed profile-details

--- src/main/resources/templates/restClient/src/app/add-profile/add-profile.component.ts
+++ src/main/resources/templates/restClient/src/app/add-profile/add-profile.component.ts
@@ -6,7 +6,7 @@ import { ProfileService }  from '../profile.service';
 import { Profile } from '../models/profile';
 
 @Component({
-  selector: 'add-profile-detail',
+  selector: 'app-add-profile',
   templateUrl: './add-profile.component.html',
   styleUrls: ['./add-profile.component.css']
 })
