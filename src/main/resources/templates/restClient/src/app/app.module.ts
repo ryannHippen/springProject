084265e64d891c69f4b7ad8942307e85109329fe
Owen Koehler <owenakoehler@gmail.com>
2019-10-22 10:52:24 -0400

    backing up before adding flex-layout

--- src/main/resources/templates/restClient/src/app/app.module.ts
+++ src/main/resources/templates/restClient/src/app/app.module.ts
@@ -3,18 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { ProfilesComponent } from './profiles/profiles.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
 import { AddProfileComponent } from './add-profile/add-profile.component';
-// import { EvaluationComponent } from './evaluation/evaluation.component';
 import { EvaluationsComponent } from './evaluations/evaluations.component';
 import { AddEvaluationComponent } from './add-evaluation/add-evaluation.component';
 import { EvaluationDetailComponent } from './evaluation-detail/evaluation-detail.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { ProfileSearchComponent } from './profile-search/profile-search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -31,9 +35,13 @@ import { ProfileSearchComponent } from './profile-search/profile-search.componen
   imports: [
     BrowserModule,
     FormsModule,
-    // AppRoutingModule,
     HttpClientModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
